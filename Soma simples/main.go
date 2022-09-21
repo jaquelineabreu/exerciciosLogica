0bfc10b58cb730b7a034cb3c38a0d097f4145325
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2022-09-20 22:06:52 -0300

    resolução em go de soma simples

---
+++ Soma simples/main.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main() {
+	var valor1, valor2, soma int
+	fmt.Scanf("%d\n",&valor1)
+	fmt.Scanf("%d\n",&valor2)
+	soma =  valor1 + valor2
+    fmt.Println("SOMA = ",soma)
+}
