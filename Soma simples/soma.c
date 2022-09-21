4f85fb84cf960ac8a2d73f39222368ee886e31db
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2022-09-20 21:38:22 -0300

    resolução em c

---
+++ Soma simples/soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int valor1, valor2, soma;
+    
+    scanf("%i",&valor1);
+    scanf("%i",&valor2);
+    printf("SOMA = %i\n", valor1 + valor2);
+
+    return 0;
+}
